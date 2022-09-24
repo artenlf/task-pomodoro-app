0ae2d8bf8232dc1f50357802c01c61e208eb4fed
artenlf <felipe.arten@gmail.com>
2022-09-24 13:24:25 -0300

    Adds Column styled components

--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+import Task from "../Task/Task";
+
+const Container = styled.div`
+  background-color: var(--dark-blue);
+  border: 2px solid var(--light-grey);
+  border-radius: 0.7rem;
+  margin: 0.5rem;
+  width: 200px;
+`;
+const Title = styled.h3`
+  color: var(--light-blue);
+  margin: 0.5rem 1.5rem;
+  padding: 0.5rem;
+`;
+const TaskList = styled.div`
+  color: var(--green);
+  margin: 0.5rem 2rem;
+  padding: 0.5rem;
+`;
 
 function Column(props) {
-  return <div>{props.column.title}</div>;
+  return (
+    <Container>
+      <Title>{props.column.title}</Title>
+      <TaskList>
+        {props.tasks.map((task) => (
+          <Task key={task.id} task={task} />
+        ))}
+      </TaskList>
+    </Container>
+  );
 }
 
 export default Column;
