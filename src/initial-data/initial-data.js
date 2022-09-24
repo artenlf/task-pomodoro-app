a486838ceed47d7bb1935b51198ed09112cb3986
artenlf <felipe.arten@gmail.com>
2022-09-24 13:25:38 -0300

    fix typo on task-4

--- src/initial-data/initial-data.js
+++ src/initial-data/initial-data.js
@@ -3,7 +3,7 @@ const initialData = {
     "task-1": { id: "task-1", content: "Pay the bills" },
     "task-2": { id: "task-2", content: "Check daily meetings" },
     "task-3": { id: "task-3", content: "Study GraphQL" },
-    "task-4": { id: "task-4", contet: "Read about project patterns" },
+    "task-4": { id: "task-4", content: "Read about project patterns" },
     "task-5": { id: "task-5", content: "Buy pasta for the dinner" },
   },
   columns: {
